167de98c0f893ac8dfb3044be8f26862ff92727d
Daniel <danielhunterteo@gmail.com>
2022-02-17 17:59:07 -0300

    Criado Metodo Comprimentar e atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,17 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 35):
+        self.nome = nome
+        self.idade= idade
+
     def comprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Daniel')
     print(Pessoa.comprimentar(p))
     print(id(p))
     print(p.comprimentar())
+    print(p.nome)
+    print(p.idade)
 
 
